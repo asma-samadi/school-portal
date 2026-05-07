c24cab60d6aa346ef907372633118ddb78bb773e
Asma Samadi <asmasamadi001@gmail.com>
2026-05-07 17:50:37 +0430

    Add css to btn in index.html

--- script.js
+++ script.js
@@ -35,7 +35,8 @@ function displayStudents() {
 // Check condition and Show the welcome message
 let editIndex = null;
 
-enrollButton.addEventListener("click", function () {
+enrollButton.addEventListener("click", function (event) {
+  event.preventDefault;
   let student = {
     name: name.value,
     age: age.value,
@@ -92,64 +93,4 @@ allResult.addEventListener("click", function (e) {
     age.value = student.age;
     grade.value = student.grade;
   }
-});
-
-// Show user info on profile page
-let container = document.querySelector("#user-info");
-
-let studentInfo = [
-  {
-    name: "Suda",
-    age: 23,
-    grade: 2,
-    photo: "images/suda.jpg",
-    email: "suda99@gmail.com",
-    phone: "0789654321",
-    status: "Active",
-  },
-  {
-    name: "Salwa",
-    age: 33,
-    grade: 12,
-    photo: "images/salwa.jpg",
-    email: "salwa88@gmail.com",
-    phone: "0709784321",
-    status: "Active",
-  },
-  {
-    name: "Shila",
-    age: 29,
-    grade: 11,
-    photo: "images/shila.jpg",
-    email: "shila@gmail.com",
-    phone: "0798765439",
-    status: "Suspended",
-  },
-];
-
-// random student
-let randomIndex = Math.floor(Math.random() * studentInfo.length);
-let info = studentInfo[randomIndex];
-
-// run after page loads
-window.onload = function () {
-  container.innerHTML = `
-    <img src="${info.photo}" width="120" style="border-radius:50%" />
-    <h2>${info.name}</h2>
-    <p>Age: ${info.age}</p>
-    <p>Grade: ${info.grade}</p>
-    <p>Email: ${info.email}</p>
-    <p>Phone: ${info.phone}</p>
-    <p>Status: ${info.status}</p>
-`;
-};
-
-// contact Information
-let showEmail = document.querySelector("#show-email");
-let showPhone = document.querySelector("#show-phone");
-let hideContact = document.querySelector("#hide-info");
-let contact = document.querySelector("#contact-info");
-
-showEmail.addEventListener("click", function () {
-  contact.innerHTML = `<h2>Email: ${info.email}</h2>`;
 });
